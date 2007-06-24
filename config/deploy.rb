ac0ba24337663ea3ae10284b1396525e4f06b40a
Gareth Andrew <gingerhendrix@hotmail.com>
2007-06-24 23:46:02 +0000

    Recapistranizing
    git-svn-id: svn+ssh://s1439.gridserver.com/home/1439/users/.home/repos/tofu/head@119 5d58421e-2428-0410-a994-821f96727b03

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,31 +1,41 @@
-
 require 'mt-capistrano'
 
-set :site,         "1439" #Media-temple user-id thingy as in /home/1439/containers etc.
+set :site,         "1439"
 set :application,  "tofu"
 set :webpath,      "tofu.gandrew.com"
 set :domain,       "gandrew.com"
 set :user,         "serveradmin%gandrew.com"
 set :password,     "enichem42"
-
-ssh_options[:username] = 'serveradmin%gandrew.com'
 ssh_options[:password] = "enichem42"
+set :svn_password, "enichem42"
 
-set :repository, "svn+ssh://#{user}@#{domain}/home/#{site}/users/.home/repos/#{application}/head"
-set :deploy_to,  "/home/#{site}/containers/rails/#{application}"
+# repository on (gs)
+set :repository, "svn+ssh://#{user}@#{domain}/home/#{site}/users/.home/repos/tofu/head"
 
-set :checkout, "export"
+#full path to svn
+#set :svn, "/C:/subversion/bin/svn.exe"
 
+# repository elsewhere
+#set :repository, "svn+ssh://user@other.com/usr/local/svn/repo/app1/trunk"
+#set :repository, "https://other.com/usr/local/svn/repo/app1/trunk"
+
+
+# these shouldn't be changed
 role :web, "#{domain}"
 role :app, "#{domain}"
 role :db,  "#{domain}", :primary => true
+set :deploy_to,    "/home/#{site}/containers/rails/#{application}"
 
-task :after_update_code, :roles => :app do
-  put(File.read('config/database.yml'), "#{release_path}/config/database.yml", :mode => 0444)
+#set :migrate_env, "VERSION=0"
+
+#task :after_update_code, :roles => :app do
+#  put(File.read('deploy/database.yml.mt'), "#{release_path}/config/database.yml", :mode => 0444)
+#end
+
+task :after_symlink, :roles => :app do
+  run "#{mtr} -u #{user} -p #{password} generate_htaccess #{application}"
 end
 
-task :restart, :roles => :app do
-  run "mtr restart #{application} -u #{user} -p #{password}"
-  run "mtr generate_htaccess #{application} -u #{user} -p #{password}"
-  migrate
+task :after_update_code, :roles => [:web] do
+  run "cp /home/#{site}/containers/rails/#{application}/shared/system/config/database.yml #{release_path}/config/database.yml"
 end
